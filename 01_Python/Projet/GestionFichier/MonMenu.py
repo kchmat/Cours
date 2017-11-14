8c9e1dabd8fa1814c5ada397dce3f6ee1efa90a1
kchmat <eloualid.elalaoui@gmail.com>
2017-11-14 14:31:10 +0100

    Create MonMenu.py

---
+++ 01_Python/Projet/GestionFichier/MonMenu.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import datetime
+import os
+#definition de la fonction qui affiche mon menu
+
+def PrintMenu():
+    print("please inserer une commande pour continue");
+    print("1\ E pour Exporter");
+    print("2\ I pour effacer l'ecran");
+    print("3\ A pour ecrire dans un fichier /tmp/aminmodule.txt");
+    print("4\ Q pour quitter le programme");
+
+
+#definition de la fonction qui crée un fichier
+def EcrireFichier(monchemin="default.txt" ):
+    fichier = open(monchemin, "w");
+    fichier.write("Amine M9Awadeeeee moncontenu");
+    fichier.close();
+
+
+def changerdir(path='/tmp/'):
+    os.chdir("{0}".format(path))
+
+
+today =datetime.date.today();
+print(today)
+#k=raw_input("entrer une lettre:")
+#print(k)
+PrintMenu()
+b=True;
+while b==True:
+    a=raw_input(">");
+    if a=="Q":
+        #b=False;
+        break;
+    elif a=="E":
+        e=raw_input("please entrer le nom de fichier a créer:")
+        changerdir();
+        EcrireFichier(a)
+    else:
+        PrintMenu()
+print("c'est bon c'est plus dans le menu des commande");
