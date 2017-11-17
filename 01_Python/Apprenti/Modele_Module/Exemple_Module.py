c5ef280813df50072c307d2b00755e4b56dffd86
kchmat <eloualid.elalaoui@gmail.com>
2017-11-17 13:39:06 +0100

    Update Exemple_Module.py

--- 01_Python/Apprenti/Modele_Module/Exemple_Module.py
+++ 01_Python/Apprenti/Modele_Module/Exemple_Module.py
@@ -1,1 +1,17 @@
 #!/etc/bin/python
+from ansible.module_utils.basic import *
+
+
+#################################################main() is the entrypoint into your module.
+def main():
+  
+#AnsibleModule comes from from ansible.module_utils.basic import *. It has to be imported with the *
+
+    module = AnsibleModule(argument_spec={})
+    response = {"hello": "world"}
+    module.exit_json(changed=False, meta=response)
+#AnsibleModule helps us handle incoming parameters and exiting the program (module.exit_json())
+
+
+if __name__ == '__main__':  
+    main()
