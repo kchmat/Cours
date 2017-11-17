db670641739ece673e70b5389386fc6f29490c48
kchmat <eloualid.elalaoui@gmail.com>
2017-11-17 13:55:53 +0100

    Update exemple.py

--- 01_Python/Apprenti/Modele_Module/library/exemple.py
+++ 01_Python/Apprenti/Modele_Module/library/exemple.py
@@ -1,3 +1,24 @@
 #!/etc/bin/python
 from ansible.module_utils.basic import *
 def main():
+   module = AnsibleModule(
+        argument_spec=dict(
+            path=dict(required=True),
+        ),
+        supports_check_mode=True
+    )
+     Retrieving parameters value
+    path = module.params['path']
+
+    # Check if paths are valid
+
+    if not os.path.exists(path):
+        os.system("mkdir {0}".format(path))
+        module.exit_json(changed=True, msg=path+"est cree")
+
+    else:
+        module.fail_json(msg=path+" exists")
+
+
+if __name__ == '__main__':
+    main()
