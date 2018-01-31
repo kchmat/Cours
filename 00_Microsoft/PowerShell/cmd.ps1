336c852f4e811ee5fa362d6f59b290c516b31ab4
kchmat <eloualid.elalaoui@gmail.com>
2018-01-31 14:32:10 +0100

    Update cmd.ps1

--- 00_Microsoft/PowerShell/cmd.ps1
+++ 00_Microsoft/PowerShell/cmd.ps1
@@ -3,3 +3,14 @@ get-command
 set-wmiobject
 move-adcomputer
 read-host
+get-help <nom de la commande>
+get-command 
+get-member (gm)
+
+========================================Variables================================
+
+casting de variable
+$re=[String]$re
+$re=[int]$re
+
+====================================================================================
